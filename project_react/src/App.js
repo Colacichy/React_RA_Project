6cf9f1773e9b320fca312b34ddfd5c938ea35e0d
oc2006 <Oc2006@DOM.SCI.EDU.PL>
2024-10-02 08:59:07 +0200

    first commit

---
+++ project_react/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Sci from './Sci';
+
+function App() {
+  return (
+    <>
+    <div className="App">
+      <h1>Hello World</h1>
+    </div>
+    <div>
+      <p>adadads</p>
+    </div>
+    <Sci/>
+    </>
+  );
+}
+
+export default App;
---
+++ project_react/src/Sci.js
@@ -0,0 +1,10 @@
+
+function Sci() {
+    return(
+        <div>
+            <p>Tekst sci</p>
+        </div>
+    );
+}
+
+export default Sci;
