dddac7d789d31958eec255f904c5935ceede75e2
Colacichy <czajkowskiolivier@gmail.com>
2024-10-30 08:34:12 +0100

    Added first endpoints

--- project_backend/main.js
+++ project_backend/main.js
@@ -7,4 +7,41 @@ app.get('/', (req, res) => {
   res.send('hello world')
 });
 
-app.listen(8080);
+
+app.get('/abc', (req, res) => {
+  res.statusCode = 500
+  res.send('hello world')
+});
+
+// sci / 4c / abc
+
+const sciRouter = express.Router()
+
+sciRouter.get('/', (req, res) => {
+  res.send('Sci')
+})
+
+sciRouter.get('/json', (req, res) => { 
+  const data = [{
+    key1: "text",
+    key2: 20
+  }]
+  res.json(data)
+})
+
+app.post('/abc', (req, res) => {
+  res.statusCode = 500
+  res.send('hello world cos innego')
+});
+
+app.delete('/abc', (req, res) => {
+  res.statusCode = 500
+  res.send('hello world cos innego ale do usuniecia')
+});
+
+app.listen(8080, () =>{
+  console.log("Cokolwiek");
+});
+
+
+app.use('/sci/4c/abc', sciRouter)
